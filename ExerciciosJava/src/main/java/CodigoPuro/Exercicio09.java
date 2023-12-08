fd2d594571859f69b7a46efca7a532fba112c8f3
Julio Alves da Silva Jr <julioadsj@outlook.com>
2023-12-08 19:50:43 -0300

    Exercicio 09 - Codigo Puro (Correção)

--- ExerciciosJava/src/main/java/CodigoPuro/Exercicio09.java
+++ ExerciciosJava/src/main/java/CodigoPuro/Exercicio09.java
@@ -13,10 +13,20 @@ public class Exercicio09 { //Desafio 09: Faça um programa que leia um número i
         System.out.println("Bem vindo a tabuada da Alexa!");
         //Enquanto o loop WHILE estiver ative, podemos solicitar varios numeros ao input
         while (chave){
-
-            //Armazenando numero do input a uma variavel int
-            System.out.print("Digite um numero para a tabuada: (ou digite 0 para sair) ");
-            int num = numeroInput.nextInt();
+            //Variavel irá conter o numero digitado no input, e o mesmo será substituido a cada loop
+            int num=0;
+            
+            //Mecanica de Erro
+            while (true){
+                try{
+                    //Armazenando numero do input a uma variavel int
+                    System.out.print("Digite um numero para a tabuada: (ou digite 0 para sair) ");
+                    num = Integer.parseInt(numeroInput.nextLine());
+                    break;
+                }catch(NumberFormatException ERRO){
+                    System.out.println("ERRO! DIGITE UM VALOR VALIDO!");
+                }
+            }
             
             //Caso será digitado o numero 0 no input acima, o loop WHILE será encerrado
             if (num==0){
