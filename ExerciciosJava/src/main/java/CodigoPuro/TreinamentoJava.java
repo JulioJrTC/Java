9b32d2587388e739d2cb89f684e8ce9b4a8c599a
Julio Alves da Silva Jr <julioadsj@outlook.com>
2024-01-12 20:03:09 -0300

    Exercicio 16 - Interface

--- ExerciciosJava/src/main/java/CodigoPuro/TreinamentoJava.java
+++ ExerciciosJava/src/main/java/CodigoPuro/TreinamentoJava.java
@@ -6,31 +6,6 @@ public class TreinamentoJava {
     
     public static void main(String[] args){
         
-        Scanner lerNumero = new Scanner(System.in);
-    
-        String numeroString;
-        Double numeroDouble;
-        int numeroInt;
-
-        System.out.print("Digite um valor: ");
-        numeroString = lerNumero.nextLine();
-        
-        if(numeroString.contains(",")){
-            numeroString = numeroString.replace(",", ".");
-        }
-        
-        numeroDouble=Double.valueOf(numeroString);
-        numeroInt=(int)Math.floor(numeroDouble);
-        
-        
-        System.out.println(numeroString);
-        System.out.println(numeroDouble);
-        System.out.println(numeroInt);
-        
     }
-    
-    
-    
-    
 
 }
