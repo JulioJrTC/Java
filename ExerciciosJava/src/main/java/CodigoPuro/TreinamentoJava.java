36ae5ee72d5d25f2dabfc167378a016dd2398e28
Julio Alves da Silva Jr <julioadsj@outlook.com>
2024-02-14 16:45:06 -0300

    Exercicio 20 - Interface

--- ExerciciosJava/src/main/java/CodigoPuro/TreinamentoJava.java
+++ ExerciciosJava/src/main/java/CodigoPuro/TreinamentoJava.java
@@ -2,25 +2,13 @@ package CodigoPuro;
 
 import java.util.Scanner;
 import java.util.Random;
+import java.util.ArrayList;
 
 public class TreinamentoJava {
     
     public static void main(String[] args){
+                
         
-        //Criando uma array numerica de 4 numeros
-        int[] numeros = {5,4,3,9};
-        
-        //Criando um novo objeto Random
-        Random numeroRandom = new Random();
-        
-        //Gerando um index aleatorio com base no tamanho do array
-        int indexAleatorio = numeroRandom.nextInt(numeros.length);
-        
-        //Acessando o valor aleatorio do array
-        int escolhaAleatorio = numeros[indexAleatorio];
-        
-        //Imprimindo o resultado
-        System.out.println(escolhaAleatorio);
         
     }
 
